e92d5ac54412f4128507d1c36a8755cb0b36d547
haorao <haoran@izhimu.cn>
2024-04-08 10:30:51 +0800

    feat: 新增图片缩略图转换

--- src-tauri/src/file/image_scanner.rs
+++ src-tauri/src/file/image_scanner.rs
@@ -1,10 +1,39 @@
-use crate::file::scan::Scanner;
+use std::io::Cursor;
+use std::path::Path;
+
+use base64::{engine::general_purpose, Engine as _};
 use chrono::offset::Utc;
 use chrono::DateTime;
 use image::imageops::FilterType;
-use image::{GenericImageView, ImageFormat};
-use std::fmt::Display;
-use std::path::Path;
+use image::{DynamicImage, GenericImageView, ImageFormat};
+
+use crate::file::scan::Scanner;
+
+fn resize_to_base64(image: DynamicImage, w: u32, h: u32) -> Option<String> {
+    let w1 = 200;
+    let h1 = (200f32 / w as f32 * h as f32) as u32;
+    let small = image.resize(w1, h1, FilterType::Triangle);
+    let mut buffer = Vec::new();
+    if small
+        .write_to(&mut Cursor::new(&mut buffer), ImageFormat::Jpeg)
+        .is_ok()
+    {
+        let base64 = general_purpose::STANDARD.encode(&buffer);
+        return Some(format!("data:image/jpg;base64,{}", base64));
+    }
+    None
+}
+
+pub struct ImageMetadata {
+    file_path: String,
+    file_name: String,
+    file_size: u64,
+    file_suffix: String,
+    created: String,
+    modified: String,
+    image_width: u32,
+    image_height: u32,
+}
 
 pub struct ImageScanner {}
 
@@ -25,8 +54,10 @@ impl Scanner for ImageScanner {
                         if let Some(file_name) = path.file_name() {
                             println!("文件名：{}", file_name.to_string_lossy());
                         }
+                        let mut file_size: u64 = 0;
                         if let Ok(metadata) = path.metadata() {
-                            println!("文件大小：{}", metadata.len());
+                            file_size = metadata.len();
+                            println!("文件大小：{}", file_size);
                             println!("格式：{:?}", file_suffix);
                             if let Ok(time) = metadata.created() {
                                 let datetime: DateTime<Utc> = time.into();
@@ -38,18 +69,13 @@ impl Scanner for ImageScanner {
                             }
                         }
                         if let Ok(image) = image::open(path) {
-                            // let dimensions = image.dimensions();
-                            // let w = dimensions.0;
-                            // let h = dimensions.1;
-                            // println!("尺寸：{} * {}", w, h);
-                            // let small = image.resize(200, 200 / w * h, FilterType::Nearest);
-                            // small.save(
-                            //     format!(
-                            //         "/home/haoran/temp/{}.png",
-                            //         path.file_name().unwrap().to_str().unwrap()
-                            //     )
-                            // );
-                            // println!("缩咯图：{} * {}", dimensions.0, dimensions.1);
+                            let dimensions = image.dimensions();
+                            let w = dimensions.0;
+                            let h = dimensions.1;
+                            println!("尺寸：{} * {}", w, h);
+                            if let Some(base64) = resize_to_base64(image, w, h) {
+                                println!("缩咯图：{}", base64)
+                            }
                         }
                         return true;
                     }
