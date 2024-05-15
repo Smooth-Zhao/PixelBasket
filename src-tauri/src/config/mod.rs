1b706aeb9d46101ed30de6d3eeb6f5b27fae26b7
haorao <haoran@izhimu.cn>
2024-05-15 16:56:07 +0800

    feat: 增加日志输出

--- src-tauri/src/config/mod.rs
+++ src-tauri/src/config/mod.rs
@@ -1,3 +1,4 @@
+use crate::debug;
 use tauri::AppHandle;
 
 pub static mut DB: String = String::new();
@@ -8,8 +9,10 @@ pub fn get_db_path() -> &'static str {
 
 pub fn set_db_path(app_handle: AppHandle) {
     if let Some(resource) = app_handle.path_resolver().resolve_resource("db/main.db") {
-        if let Some(str) = resource.to_str() {
-            unsafe { DB = str.to_string() }
+        if let Some(path) = resource.to_str() {
+            let path = path.to_string();
+            debug!("DB path: {}", &path);
+            unsafe { DB = path }
         }
     }
 }
